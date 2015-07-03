f833c379d2adfba2c8e1c82496066ac6a6cebeb1
Chris Berry <clberry7107@yahoo.com>
2015-07-03 13:59:23 +0000

    added user model and functionality :

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :articles do
-    resources :comments
+  resources :users 
+   resources :articles do
+     resources :comments
   end
  
   root 'welcome#index'
